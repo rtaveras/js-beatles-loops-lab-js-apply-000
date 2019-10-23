325b1d4b539897c233354ec579665aed1d0cfdee
Riaan Taveras <riaantaveras@gmail.com>
2019-10-23 03:04:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-
 function theBeatlesPlay(musicians, instruments)
 {
-  var music = [];
+  var counter = [];
+  for (var x = 0; x < 4; x++)
+  counter.push (musicians[x] +' plays ' + instruments[x]);
+  return counter;
+}
+
+function johnLennonFacts(facts)
+{
+  var i = 0;
+  while (i < facts.length){
+    facts[i] = facts[i] + '!!!';
+  i++;
+  
+  }
+  return facts;
+}
+
+
+function iLoveTheBeatles(n)
+{
+  var emptyArray = [];
+  do {emptyArray.push('I love the Beatles!');
+  n++;}
+  while(n < 15);
+  return emptyArray;
 }
